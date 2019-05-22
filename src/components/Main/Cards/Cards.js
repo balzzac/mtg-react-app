da20d495bb983a05d739449848457ff52f6191a1
Maria Terekhina <terekhina.maria@gmail.com>
2019-05-21 21:02:34 -0700

    dev: Cards component

---
+++ src/components/Main/Cards/Cards.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Card from './Card/Card';
+
+import './Cards.css';
+
+/**
+ * Cards react component
+ * @param {object} props - properties sent through Main component
+ * @return {jsx}
+ */
+export default function Cards(props) {
+  console.log(props);
+  return (
+    <div className="l-cards">
+      {props.cards.map((card) => (
+        <Card key={card.id} card={card} />
+      ))}
+    </div>
+  );
+}
+
+Cards.propTypes = {
+  card: PropTypes.object,
+  cards: PropTypes.array,
+};
